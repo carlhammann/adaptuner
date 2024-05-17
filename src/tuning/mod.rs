58e0e4b739a8760f2e894ea7b33b26f8ccab9342
Carl Hammann <carl@carlhammann.com>
2024-05-17 18:18:01 +0200

    Remove unused module

--- src/tuning/mod.rs
+++
@@ -1,9 +0,0 @@
-use ndarray::Array1;
-
-use crate::interval::Stack;
-
-// pub struct State<'a> {
-//     stacks: [Stack<'a>; 12],
-//     reference: Stack<'a>,
-//     active_temperaments: Array1<bool>,
-// }
