d0494b82554b9c93c066561dfedcd9dc1a7b9828
Carl Hammann <carl@carlhammann.com>
2025-03-10 22:34:46 +0100

    Use the LU decomposition in spring solver
    The last test case still overflows...

--- src/process/springs/util.rs
+++ src/process/springs/util.rs
@@ -7,9 +7,12 @@ use ndarray::{s, Array1, Array2, ArrayView1, ArrayView2};
 use num_rational::Ratio;
 
 use super::solver;
-use crate::interval::{
-    base::Semitones,
-    stacktype::r#trait::{StackCoeff, StackType},
+use crate::{
+    interval::{
+        base::Semitones,
+        stacktype::r#trait::{StackCoeff, StackType},
+    },
+    util::lu,
 };
 
 pub enum Connector {
@@ -83,11 +86,14 @@ struct Workspace<T: StackType> {
     _phantom: PhantomData<T>,
     memo_intervals: bool,
     memo_note_anchors: bool,
+    n_keys: usize,
     key_distances: Array2<KeyDistance>,
     candidates: HashMap<KeyOrDistance, (Array2<Ratio<StackCoeff>>, Array1<Ratio<StackCoeff>>)>,
     current_connections: BTreeMap<ConnectedKeys, IntervalInfo>,
-    pub best_solution: Array2<Ratio<StackCoeff>>,
-    pub best_energy: Semitones,
+    best_solution: Array2<Ratio<StackCoeff>>,
+    best_energy: Semitones, // May not be exactly zero even if `relaxed` is true, due to
+    // floating point imprecisions
+    relaxed: bool,
 }
 
 impl<T: StackType> Workspace<T> {
@@ -102,12 +108,30 @@ impl<T: StackType> Workspace<T> {
             _phantom: PhantomData,
             memo_intervals,
             memo_note_anchors,
+            n_keys: 0,
             key_distances: Array2::zeros((initial_n_keys, initial_n_keys)),
             candidates: HashMap::new(),
             current_connections: BTreeMap::new(),
             best_solution: Array2::zeros((initial_n_keys, T::num_intervals())),
             best_energy: Semitones::MAX,
+            relaxed: false,
         }
+    }
+
+    /// Call [Self::compute_best_solution] first.
+    pub fn best_solution(&self) -> ArrayView2<Ratio<StackCoeff>> {
+        self.best_solution
+            .slice(s![..self.n_keys, ..T::num_intervals()])
+    }
+
+    /// Call [Self::compute_best_solution] first.
+    pub fn best_energy(&self) -> Semitones {
+        self.best_energy
+    }
+
+    /// Call [Self::compute_best_solution] first.
+    pub fn relaxed(&self) -> bool {
+        self.relaxed
     }
 
     ///  The ordering of `keys` matters: Notes that come earlier (and the intervals between them)
@@ -120,13 +144,14 @@ impl<T: StackType> Workspace<T> {
         provide_candidate_intervals: PI,
         provide_candidate_notes: PN,
         solver_workspace: &mut solver::Workspace,
-    ) -> Result<(), fractionfree::LinalgErr>
+    ) -> Result<(), lu::LUErr>
     where
         WC: Fn(KeyNumber, KeyNumber) -> Connector,
         AP: Fn(KeyNumber) -> bool,
         PI: Fn(KeyDistance) -> (Array2<Ratio<StackCoeff>>, Array1<Ratio<StackCoeff>>),
         PN: Fn(KeyNumber) -> (Array2<Ratio<StackCoeff>>, Array1<Ratio<StackCoeff>>),
     {
+        self.n_keys = keys.len();
         self.collect_rod_and_spring_key_distances(&keys, which_connector);
         let next_index =
             self.collect_relative_intervals_and_connections(provide_candidate_intervals);
@@ -143,7 +168,7 @@ impl<T: StackType> Workspace<T> {
         self.best_energy = Semitones::MAX;
 
         self.solve_current_candidate(solver_workspace)?;
-        while self.prepare_next_candidate() {
+        while !self.relaxed & self.prepare_next_candidate() {
             self.solve_current_candidate(solver_workspace)?;
         }
         Ok(())
@@ -177,8 +202,8 @@ impl<T: StackType> Workspace<T> {
     fn solve_current_candidate(
         &mut self,
         solver_workspace: &mut solver::Workspace,
-    ) -> Result<(), fractionfree::LinalgErr> {
-        let n_nodes = self.key_distances.shape()[0];
+    ) -> Result<(), lu::LUErr> {
+        let n_nodes = self.n_keys;
         let n_lengths = self.current_connections.len();
         let n_base_lengths = T::num_intervals();
 
@@ -232,6 +257,7 @@ impl<T: StackType> Workspace<T> {
         let solution = system.solve()?;
 
         let energy = self.energy_in(solution.view());
+        let relaxed = self.relaxed_in(solution.view());
 
         println!("solution:\n{}", solution);
         println!("energy: {}\n\n\n", energy);
@@ -241,9 +267,12 @@ impl<T: StackType> Workspace<T> {
             self.best_solution = Array2::zeros(solution.raw_dim());
         }
 
-        if energy < self.best_energy {
-            self.best_solution.assign(&solution);
+        if relaxed | (energy < self.best_energy) {
+            self.best_solution
+                .slice_mut(s![..self.n_keys, ..T::num_intervals()])
+                .assign(&solution);
             self.best_energy = energy;
+            self.relaxed = relaxed;
         }
 
         Ok(())
@@ -278,8 +307,11 @@ impl<T: StackType> Workspace<T> {
         self.get_semitones_in(j, solution) - self.get_semitones_in(i, solution)
     }
 
-    /// Compute the energy stored in tensioned springs (== detuned intervals) in the provided
-    /// solution.
+    /// Compute the energy stored in tensioned springs (== detuned intervals or notes) in the
+    /// provided solution.
+    ///
+    /// Don't compare this number to zero to find out if there are detunings; use
+    /// [Self::relaxed_in] for that purpose!
     fn energy_in(&self, solution: ArrayView2<Ratio<StackCoeff>>) -> Semitones {
         let mut res = 0.0;
 
@@ -327,6 +359,48 @@ impl<T: StackType> Workspace<T> {
         }
 
         res
+    }
+
+    /// returns true iff all springs have their relaxed length (that is: there are no detuned
+    /// intervals or notes) in the provided solution.
+    fn relaxed_in(&self, solution: ArrayView2<Ratio<StackCoeff>>) -> bool {
+        let target_coefficients = |d: &KeyOrDistance, i: usize| {
+            let (intervals, _) = self
+                .candidates
+                .get(d)
+                .expect("relaxed_in: no candidate intervals found for key distance. This should never happen.");
+            intervals.row(i)
+        };
+
+        for (keys, conn) in self.current_connections.iter() {
+            match keys {
+                ConnectedKeys::Rod(_, _) => {}
+                ConnectedKeys::Spring(i, j) => {
+                    let d = &conn.key_or_distance;
+                    let ci = conn.current_candidate_index;
+                    let l = target_coefficients(d, ci);
+
+                    for k in 0..T::num_intervals() {
+                        if l[k] != solution[[*j, k]] - solution[[*i, k]] {
+                            return false;
+                        }
+                    }
+                }
+                ConnectedKeys::FixedSpring(i) => {
+                    let d = &conn.key_or_distance;
+                    let ci = conn.current_candidate_index;
+                    let l = target_coefficients(d, ci);
+
+                    for k in 0..T::num_intervals() {
+                        if l[k] != solution[[*i, k]] {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        true
     }
 
     /// expected invariants:
@@ -420,7 +494,7 @@ impl<T: StackType> Workspace<T> {
             }
         };
 
-        let n = self.key_distances.shape()[0];
+        let n = self.n_keys;
 
         for i in 0..n {
             for j in (i + 1)..n {
@@ -524,7 +598,6 @@ impl<T: StackType> Workspace<T> {
 #[cfg(test)]
 mod test {
     use ndarray::{arr1, arr2, s};
-    use num_traits::Euclid;
     use pretty_assertions::assert_eq;
 
     use crate::interval::stacktype::fivelimit::ConcreteFiveLimitStackType;
@@ -637,8 +710,14 @@ mod test {
                     arr1(&[Ratio::new(1, 4 * 5)]),
                 ),
                 9 => (
-                    arr2(&[[octaves + 1, (-1).into(), 1.into()]]), // major sixth
-                    arr1(&[Ratio::new(1, 2 * 3 * 4 * 5)]),
+                    arr2(&[
+                        [octaves + 1, (-1).into(), 1.into()], // major sixth
+                        [octaves - 1, 3.into(), 0.into()],    // major tone plus fifth
+                    ]),
+                    arr1(&[
+                        Ratio::new(1, 2 * 3 * 4 * 5),
+                        Ratio::new(1, 2 * 3 * 2 * 3 * 2 * 3),
+                    ]),
                 ),
                 10 => (
                     arr2(&[
@@ -662,6 +741,8 @@ mod test {
 
         let provide_candidate_notes = |i| provide_candidate_intervals(i as KeyDistance - 60);
 
+        let epsilon = 0.00000000000000001; // just a very small number. I don't care precisely.
+
         // if nothing else is given, the first option is picked
         ws.compute_best_solution(
             &[60, 66],
@@ -673,13 +754,14 @@ mod test {
         )
         .unwrap();
         assert_eq!(
-            ws.best_solution,
+            ws.best_solution(),
             arr2(&[
                 [0.into(), 0.into(), 0.into()],
                 [(-1).into(), 2.into(), 1.into()], // tritone as major tone plus major third
             ])
         );
-        assert_eq!(ws.best_energy, 0.0);
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
 
         // no new interval, so `provide_candidate_intervals` is never called.
         ws.compute_best_solution(
@@ -692,13 +774,56 @@ mod test {
         )
         .unwrap();
         assert_eq!(
-            ws.best_solution,
+            ws.best_solution(),
             arr2(&[
                 [0.into(), 0.into(), 0.into()],
                 [(-1).into(), 2.into(), 1.into()], // tritone as major tone plus major third
             ])
         );
-        assert_eq!(ws.best_energy, 0.0);
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed);
+
+        // C major triad
+        ws.compute_best_solution(
+            &[60, 64, 67],
+            |i| i == 60,
+            |_, _| Connector::Spring,
+            provide_candidate_intervals,
+            provide_candidate_notes,
+            &mut solver_workspace,
+        )
+        .unwrap();
+        assert_eq!(
+            ws.best_solution(),
+            arr2(&[
+                [0.into(), 0.into(), 0.into()],
+                [0.into(), 0.into(), 1.into()],
+                [0.into(), 1.into(), 0.into()],
+            ])
+        );
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
+
+        // E major triad
+        ws.compute_best_solution(
+            &[64, 68, 71],
+            |i| i == 64,
+            |_, _| Connector::Spring,
+            provide_candidate_intervals,
+            provide_candidate_notes,
+            &mut solver_workspace,
+        )
+        .unwrap();
+        assert_eq!(
+            ws.best_solution(),
+            arr2(&[
+                [0.into(), 0.into(), 1.into()],
+                [0.into(), 0.into(), 2.into()],
+                [0.into(), 1.into(), 1.into()],
+            ])
+        );
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
 
         // The three notes C,D,E: Because they are mentioned in this order, the interval C-D will
         // be the major tone. See the next example as well.
@@ -712,14 +837,15 @@ mod test {
         )
         .unwrap();
         assert_eq!(
-            ws.best_solution,
+            ws.best_solution(),
             arr2(&[
                 [0.into(), 0.into(), 0.into()],
                 [(-1).into(), 2.into(), 0.into()],
                 [0.into(), 0.into(), 1.into()]
             ])
         );
-        assert_eq!(ws.best_energy, 0.0);
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
 
         // This is the same as before, but illustrates the relevance of the order in the `keys`
         // argument: Now, the tuning that makes the step from C to D a minor tone is preferred.
@@ -736,14 +862,15 @@ mod test {
         )
         .unwrap();
         assert_eq!(
-            ws.best_solution,
+            ws.best_solution(),
             arr2(&[
                 [0.into(), 0.into(), 1.into()],
                 [1.into(), (-2).into(), 1.into()],
                 [0.into(), 0.into(), 0.into()],
             ])
         );
-        assert_eq!(ws.best_energy, 0.0);
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
 
         // D-flat major seventh on C
         ws.compute_best_solution(
@@ -756,7 +883,7 @@ mod test {
         )
         .unwrap();
         assert_eq!(
-            ws.best_solution,
+            ws.best_solution(),
             arr2(&[
                 [0.into(), 0.into(), 0.into()],
                 [1.into(), (-1).into(), (-1).into()], // diatonic semitone
@@ -764,11 +891,12 @@ mod test {
                 [1.into(), 0.into(), (-1).into()],
             ])
         );
-        assert_eq!(ws.best_energy, 0.0);
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
 
-        // D minor seventh on C
+        // D dominant seventh on C
         ws.compute_best_solution(
-            &[60, 62, 66, 70],
+            &[60, 62, 66, 69],
             |i| i == 60,
             |_, _| Connector::Spring,
             provide_candidate_intervals,
@@ -777,7 +905,7 @@ mod test {
         )
         .unwrap();
         assert_eq!(
-            ws.best_solution,
+            ws.best_solution(),
             arr2(&[
                 [0.into(), 0.into(), 0.into()],
                 [(-1).into(), 2.into(), 0.into()],
@@ -785,104 +913,66 @@ mod test {
                 [(-1).into(), 3.into(), 0.into()],
             ])
         );
-        assert_eq!(ws.best_energy, 0.0);
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
 
-        //
-        //
-        //ws.compute_best_solution(
-        //    &[(
-        //        0,
-        //        Some((
-        //            arr2(&[[0.into(), 0.into(), 0.into()]]).view(),
-        //            arr1(&[1.into()]).view(),
-        //        )),
-        //    )],
-        //    |_| true,
-        //    |_| panic!("this should not be called"),
-        //    &mut sws,
-        //)
-        //.unwrap();
-        //assert_eq!(ws.best_solution, arr2(&[[0.into(), 0.into(), 0.into()]]));
-        //
-        //ws.compute_best_solution(
-        //    &[
-        //        (
-        //            0,
-        //            Some((
-        //                arr2(&[[0.into(), 0.into(), 0.into()]]).view(),
-        //                arr1(&[1.into()]).view(),
-        //            )),
-        //        ),
-        //        (1, None),
-        //    ],
-        //    |_| false,
-        //    |_| (arr2(&[[2.into(), 3.into(), 5.into()]]), arr1(&[1.into()])),
-        //    &mut sws,
-        //)
-        //.unwrap();
-        //assert_eq!(ws.best_energy, 0.0);
+        // a single note: this first option is choosen
+        ws.compute_best_solution(
+            &[69],
+            |i| i == 69,
+            |_, _| Connector::Spring,
+            provide_candidate_intervals,
+            provide_candidate_notes,
+            &mut solver_workspace,
+        )
+        .unwrap();
+        assert_eq!(
+            ws.best_solution(),
+            arr2(&[[1.into(), (-1).into(), 1.into()],])
+        );
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
+
+        // Chromatic cluster of three notes is bounded by a minor tone
+        ws.compute_best_solution(
+            &[60, 61, 62],
+            |i| i == 60,
+            |_, _| Connector::Spring,
+            provide_candidate_intervals,
+            provide_candidate_notes,
+            &mut solver_workspace,
+        )
+        .unwrap();
+        assert_eq!(
+            ws.best_solution(),
+            arr2(&[
+                [0.into(), 0.into(), 0.into()],
+                [1.into(), (-1).into(), (-1).into()],
+                [1.into(), (-2).into(), 1.into()],
+            ])
+        );
+        assert!(ws.best_energy() < epsilon);
+        assert!(ws.relaxed());
+
+        // 69 chord cannot be in tune
+        ws.compute_best_solution(
+            &[60, 62, 64, 67, 69],
+            |i| i == 60,
+            |_, _| Connector::Spring,
+            provide_candidate_intervals,
+            provide_candidate_notes,
+            &mut solver_workspace,
+        )
+        .unwrap();
         //assert_eq!(
-        //    ws.best_solution,
+        //    ws.best_solution(),
         //    arr2(&[
         //        [0.into(), 0.into(), 0.into()],
-        //        [2.into(), 3.into(), 5.into()]
+        //        [1.into(), (-1).into(), (-1).into()],
+        //        [(-1).into(), 2.into(), 0.into()],
         //    ])
         //);
-        //
-        //ws.compute_best_solution(
-        //    &[
-        //        (0, None),
-        //        (
-        //            1,
-        //            Some((
-        //                arr2(&[[0.into(), 0.into(), 0.into()]]).view(),
-        //                arr1(&[1.into()]).view(),
-        //            )),
-        //        ),
-        //    ],
-        //    |_| true,
-        //    |_| panic!("this should not be called"),
-        //    &mut sws,
-        //)
-        //.unwrap();
-        //assert_eq!(ws.best_energy, 0.0);
-        //assert_eq!(
-        //    ws.best_solution,
-        //    arr2(&[
-        //        [(-2).into(), (-3).into(), (-5).into()],
-        //        [0.into(), 0.into(), 0.into()],
-        //    ])
-        //);
-        //
-        //ws.compute_best_solution(
-        //    &[
-        //        (
-        //            0,
-        //            Some((
-        //                arr2(&[[0.into(), 0.into(), 0.into()]]).view(),
-        //                arr1(&[1.into()]).view(),
-        //            )),
-        //        ),
-        //        (
-        //            1,
-        //            Some((
-        //                arr2(&[[2.into(), 3.into(), 5.into()]]).view(),
-        //                arr1(&[1.into()]).view(),
-        //            )),
-        //        ),
-        //    ],
-        //    |_| false,
-        //    |_| panic!("this should not be called"),
-        //    &mut sws,
-        //)
-        //.unwrap();
-        //assert_eq!(ws.best_energy, 0.0);
-        //assert_eq!(
-        //    ws.best_solution,
-        //    arr2(&[
-        //        [0.into(), 0.into(), 0.into()],
-        //        [2.into(), 3.into(), 5.into()],
-        //    ])
-        //);
+        assert!(ws.best_energy() > epsilon);
+        assert!(!ws.relaxed());
     }
 }
