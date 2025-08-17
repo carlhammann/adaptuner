240dee87311e65cdfc64e41b0b686574075b4fa5
Carl Hammann <carl@carlhammann.com>
2025-08-18 00:23:30 +0200

    Add the wayland desktop output

--- flake.nix
+++ flake.nix
@@ -10,61 +10,85 @@
   };
 
   outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+    flake-parts.lib.mkFlake {inherit inputs;} ({withSystem, ...}: {
       systems = ["x86_64-linux" "aarch64-darwin"];
       perSystem = {
         pkgs,
         system,
         ...
       }: let
-        rust-bin = pkgs.rust-bin.stable.latest;
+        latestStableRust = pkgs: pkgs.rust-bin.stable.latest;
+        waylandPath = pkgs:
+          with pkgs;
+            lib.makeLibraryPath [
+              wayland
+              libGL
+              libxkbcommon
+            ];
       in rec {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [(import inputs.rust-overlay)];
         };
 
-        packages = {
-          default = let
-            craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (_: rust-bin.minimal);
-            commonArgs = {
-              src = ./.;
-              strictDeps = true;
-              nativeBuildInputs = [pkgs.pkg-config];
-              buildInputs =
-                (pkgs.lib.optionals pkgs.stdenv.isLinux [pkgs.alsa-lib])
-                ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-                  pkgs.darwin.apple_sdk.frameworks.CoreMIDI
-                ]);
+        packages = with pkgs; let
+          craneLib =
+            (inputs.crane.mkLib pkgs).overrideToolchain (p:
+              (latestStableRust p).minimal);
+          commonArgs = {
+            src = ./.;
+            strictDeps = true;
+            nativeBuildInputs = [pkg-config];
+            buildInputs =
+              (lib.optionals stdenv.isLinux [
+                alsa-lib
+              ])
+              ++ (lib.optionals stdenv.isDarwin [
+                darwin.apple_sdk.frameworks.CoreMIDI
+              ]);
+          };
+          adaptuner-bin = craneLib.buildPackage (
+            commonArgs // {cargoArtifacts = craneLib.buildDepsOnly commonArgs;}
+          );
+        in ({inherit adaptuner-bin;}
+          // lib.optionalAttrs stdenv.isLinux {
+            adaptuner-wayland = stdenv.mkDerivation {
+              inherit (adaptuner-bin) version pname;
+              phases = ["installPhase"];
+              installPhase = ''
+                mkdir -p $out/bin
+                patchelf \
+                  --add-rpath "${waylandPath pkgs}" \
+                  --output $out/bin/adaptuner \
+                  ${adaptuner-bin}/bin/adaptuner
+
+                mkdir -p $out/share/applications
+                cp ${./assets/adaptuner.desktop} $out/share/applications/adaptuner.desktop
+
+                mkdir -p $out/share/icons/hicolor/scalable/apps
+                cp ${./assets/icon/aplus.svg} $out/share/icons/hicolor/scalable/apps/adaptuner.svg
+              '';
             };
-          in
-            craneLib.buildPackage (
-              commonArgs // {cargoArtifacts = craneLib.buildDepsOnly commonArgs;}
-            );
-        };
+          });
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [packages.default];
+        devShells = pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+          wayland = pkgs.mkShell {
+            inputsFrom = [packages.adaptuner-bin];
+            packages = with pkgs;
+              [
+                fluidsynth
+                vmpk
 
-          packages = with pkgs;
-            [
-              fluidsynth
-              vmpk
+                # dev-y
+                (latestStableRust pkgs).rust-analyzer
+                (latestStableRust pkgs).rustfmt
+                bacon
+              ]
+              ++ lib.optionals stdenv.isLinux [alsa-utils];
 
-              # dev-y
-              rust-bin.rust-analyzer
-              rust-bin.rustfmt
-              bacon
-            ]
-            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [alsa-utils];
-
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (with pkgs;
-            pkgs.lib.optionals stdenv.isLinux [
-              wayland
-              libGL
-              libxkbcommon
-            ])}";
+            LD_LIBRARY_PATH = waylandPath pkgs;
+          };
         };
       };
-    };
+    });
 }
