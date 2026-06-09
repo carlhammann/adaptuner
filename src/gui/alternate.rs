dd24493c900b884f43bf402185a432c10acde57b
Carl Hammann <carl@carlhammann.com>
2026-06-09 15:09:20 +0200

    Bring back the side panel

--- src/gui/alternate.rs
+++ src/gui/alternate.rs
@@ -28,6 +28,8 @@ pub struct TopLevelGui<T: StackType, A: UiAdaptor<T>> {
 
     notifications: Notifications<T>,
 
+    show_side_panel: bool,
+
     lattice: LatticeWindow<T>,
     latency: LatencyWindow,
 }
@@ -38,6 +40,65 @@ impl<T: StackType + HasNoteNames, A: UiAdaptor<T>> eframe::App for TopLevelGui<T
         // own implementation of a modal window from egui_file_dialog
         let any_modal_open = false;
         // self.temperament_editor_window.is_open() || self.comma_editor_window.is_open();
+        //
+
+        egui::SidePanel::left("left panel").show_animated(ctx, self.show_side_panel, |ui| {
+            if any_modal_open {
+                ui.disable();
+            }
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.visuals_mut().collapsing_header_frame = true;
+
+                // AsStrategyPicker(&mut self.strategies).show(ui, &self.state, &self.tx);
+                //
+                // ui.separator();
+
+                self.connection_window
+                    .show_hide_button(ui, "MIDI connections");
+                // self.note_window.show_hide_button(ui, "notes");
+                // self.keyboard_control_window
+                //     .show_hide_button(ui, "keyboard controls");
+                // if self
+                //     .temperament_editor_window
+                //     .show_hide_button(ui, "temperaments")
+                // {
+                //     self.current_config = self.extract_config();
+                //     self.temperament_editor = TemperamentEditor::new();
+                // }
+                // if self.comma_editor_window.show_hide_button(ui, "commas") {
+                //     self.current_config = self.extract_config();
+                //     self.comma_editor = CommaEditor::new();
+                // }
+
+                ui.separator();
+
+                // if ui.button("save configuration").clicked() {
+                //     self.current_config = self.extract_config();
+                //     self.current_process_config = None {};
+                //     self.current_backend_config = None {};
+                //     let _ = self.tx.send(FromUi::GetCurrentProcessConfig);
+                //     let _ = self.tx.send(FromUi::GetCurrentBackendConfig);
+                //     self.config_file_dialog.as_save().open();
+                // }
+                //
+                // if ui.button("load configuration").clicked() {
+                //     self.current_config = self.extract_config();
+                //     self.current_process_config = None {};
+                //     self.current_backend_config = None {};
+                //     let _ = self.tx.send(FromUi::GetCurrentProcessConfig);
+                //     let _ = self.tx.send(FromUi::GetCurrentBackendConfig);
+                //     self.config_file_dialog.as_load().open();
+                // }
+                //
+                // ui.separator();
+
+                // AsBigControls(&mut self.lattice).show(ui);
+                //
+                // ui.separator();
+
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             self.notifications.clear_old(Instant::now());
@@ -74,15 +135,15 @@ impl<T: StackType + HasNoteNames, A: UiAdaptor<T>> eframe::App for TopLevelGui<T
             self.lattice.show(ui, &self.adaptor);
 
             ui.horizontal(|ui| {
-                // if !*show_side_panel {
-                //     if ui.button("☰").clicked() {
-                //         *show_side_panel = true;
-                //     }
-                // } else {
-                //     if ui.button("⏴").clicked() {
-                //         *show_side_panel = false;
-                //     }
-                // }
+                if !self.show_side_panel {
+                    if ui.button("☰").clicked() {
+                        self.show_side_panel = true;
+                    }
+                } else {
+                    if ui.button("⏴").clicked() {
+                        self.show_side_panel = false;
+                    }
+                }
 
                 if ui.button("🔍+").clicked() {
                     self.adaptor.config_mut().lattice.zoom *= 1.1;
@@ -117,6 +178,8 @@ impl<T: StackType + HasNoteNames, A: UiAdaptor<T>> Gui<T, A> for TopLevelGui<T, 
             backend: BackendWindow::new(adaptor.backend_config()),
             connection_window: SmallFloatingWindow::new(egui::Id::new("connection_window"), true),
             notifications: Notifications::new(),
+
+            show_side_panel: false,
 
             lattice: LatticeWindow::new(),
             latency: LatencyWindow::new(config.latency_mean_over),
