4caa2c6b449bb8cd356e1216a268ec557978d3fc
Carl Hammann <carl@carlhammann.com>
2026-07-25 01:52:13 +0200

    Remove unused function

--- src/util/list_action.rs
+++ src/util/list_action.rs
@@ -52,47 +52,4 @@ impl ListAction {
             ListAction::Deselect => panic!("cannot deselect"),
         }
     }
-
-    pub fn apply_to_old<X>(
-        self,
-        clone: impl Fn(&X) -> X,
-        vec: &mut Vec<X>,
-        selected: &mut Option<usize>,
-    ) {
-        match self {
-            ListAction::Delete(i) => {
-                vec.remove(i);
-                if let Some(j) = selected {
-                    if *j == 0 {
-                        return;
-                    }
-                    if *j >= i {
-                        *j -= 1;
-                    }
-                }
-            }
-            ListAction::SwapWithPrev(i) => {
-                vec.swap(i, i - 1);
-                if let Some(j) = selected {
-                    if *j == i {
-                        *j = i - 1;
-                    } else if *j == i - 1 {
-                        *j = i;
-                    }
-                }
-            }
-            ListAction::Select(i) => {
-                *selected = Some(i);
-            }
-            ListAction::Deselect => {
-                *selected = None {};
-            }
-            ListAction::Clone(i) => {
-                vec.push(clone(&vec[i]));
-                if let Some(j) = selected {
-                    *j = vec.len() - 1;
-                }
-            }
-        }
-    }
 }
